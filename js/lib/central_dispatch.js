a6fc1fe676e3257290aa94eee87d992c3b1fd300
Trotter Cashion <cashion@gmail.com>
2009-03-14 14:59:13 -0400

    Add CentralDispatch.receiveData

--- js/lib/central_dispatch.js
+++ js/lib/central_dispatch.js
@@ -1,10 +1,16 @@
 var CentralDispatch = function() {
     var klass = {};
+    var callbacks = {};
 
     klass.requestData = function(url, callback) {
+        callbacks[url] = callback;
         var tag = document.createElement('script');
         tag.src = url;
         document.body.appendChild(tag);
+    };
+
+    klass.receiveData = function(url, data) {
+        callbacks[url](data);
     };
 
     return klass;
--- js/test/bundle/central_dispatch_spec.js
+++ js/test/bundle/central_dispatch_spec.js
@@ -7,5 +7,22 @@ Screw.Unit(function() {
                 expect(document.body.lastChild.src).to(equal, url);
             });
         });
+
+        describe('registered to receive data from http://test.host/test.js', function() {
+            var callback, requestedUrl, storedData;
+
+            before(function() {
+                callback = function(data) { storedData = data };
+                requestedUrl = 'http://test.host/test.js';
+                CentralDispatch.requestData(requestedUrl, callback);
+            });
+
+            it('should callback when receiving data for http://test.host/test.js', function() {
+                var url = 'http://test.host/test.js';
+                var data = {hello: 'bob'};
+                CentralDispatch.receiveData(url, data);
+                expect(storedData).to(equal, data);
+            });
+        });
     });
 });
