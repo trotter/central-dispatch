f470c12f3d60bbd4830d595f76ca734fee66c951
Trotter Cashion <cashion@gmail.com>
2009-04-19 10:36:54 -0400

    More refactoring

--- js/lib/central_dispatch.js
+++ js/lib/central_dispatch.js
@@ -2,24 +2,25 @@ var CentralDispatch = function () {
     var klass = {},
         callbacks = {},
         addCallback, findCallbacks, runCallbacks,
-        Request;
+        makeRequest, RequestMap;
 
-    Request = function (url, callback, options) {
+    makeRequest = function (url, callback, options) {
         var self = {}, tag, executed = false;
         options = options || {};
 
-        addCallback(url, function (data) { 
+        self.callback = function (data) { 
             callback(data); 
-            if (tag && !executed) {
-                document.body.removeChild(tag); 
-                tag = null; 
+            if (self.tag && !executed) {
+                document.body.removeChild(self.tag); 
+                self.tag = null; 
             }
             executed = true;
-        });
+        };
 
         self.url = url;
         
         self.tag = function () {
+            var tag;
             tag = document.createElement('script');
             tag.src = url;
             tag.onerror = function () { 
@@ -34,58 +35,66 @@ var CentralDispatch = function () {
             };
             return tag;
         }();
+
+        self.addToDom = function () {
+            document.body.appendChild(self.tag);
+        };
+
+        RequestMap.add(self);
         return self;
     };
 
-    addCallback = function (url, callback) {
-        callbacks[url] = callbacks[url] || [];
-        callbacks[url].push(callback);
-    };
+    RequestMap = function () {
+        var klass = {}, requests = {}, findAllFor;
 
-    findCallbacks = function (url) {
-        var callback, regex, fullUrl;
+        findAllFor = function (url) {
+            var regex, fullUrl;
 
-        // Exit with an exact match if possible for speed
-        callback = callbacks[url];
-        if (callback) {
-            return callback;
-        }
-        // TODO: The following is innefficient in the case of many callbacks
-        regex = new RegExp(url + '$');
-        for (fullUrl in callbacks) {
-            if (callbacks.hasOwnProperty(fullUrl)) {
-                if (regex.test(fullUrl)) {
-                    return callbacks[fullUrl];
+            // Exit with an exact match if possible for speed
+            if (requests[url]) {
+                return requests[url];
+            }
+
+            // TODO: The following is innefficient in the case of many requests
+            regex = new RegExp(url + '$');
+            for (fullUrl in requests) {
+                if (requests.hasOwnProperty(fullUrl)) {
+                    if (regex.test(fullUrl)) {
+                        return requests[fullUrl];
+                    }
                 }
             }
-        }
-        // Incase we don't find anything
-        return [];
-    };
+            // Incase we don't find anything
+            return [];
+        };
 
-    runCallbacks = function (callbacks, data) {
-        var callback = callbacks.pop();
-        while (callback) {
-            // TODO: Should clone data so that functions don't spoil the fun for
-            // others.
-            callback(data); 
-            callback = callbacks.pop();
-        }
-    };
+        klass.add = function (request) {
+            requests[request.url] = requests[request.url] || [];
+            requests[request.url].push(request);
+        };
+
+        klass.runAllFor = function (url, data) {
+            var requests = findAllFor(url), current;
+            current = requests.pop();
+            while (current) {
+                // TODO: Should clone data so that functions don't spoil the fun for
+                // others.
+                current.callback(data); 
+                current = requests.pop();
+            }
+        };
+
+        return klass;
+    }();
 
     klass.requestData = function (url, callback, options) {
-        var tag, thing;
-        thing = Request(url, callback, options);
-        document.body.appendChild(thing.tag);
-        return thing.tag;
+        var request = makeRequest(url, callback, options);
+        request.addToDom();
+        return request.tag;
     };
 
     klass.receiveData = function (version, url, data) {
-        var callbacks = findCallbacks(url);
-        runCallbacks(callbacks, data);
-        if (callbacks) { 
-            callbacks[url] = null;
-        }
+        RequestMap.runAllFor(url, data);
     };
 
     return klass;
